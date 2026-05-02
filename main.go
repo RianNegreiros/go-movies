90ecc616d8712a7c520e4c09982daf0b4d593eaf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2026-05-02 07:12:58 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/riannegreiros/go-movies/logger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("go-movies.log")
+	if err != nil {
+		log.Fatalf("Could not create a log instance %v", err)
+	}
+
+	defer logInstance.Close()
+
+	return logInstance
+}
+
 func main() {
+
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Could not start server: %v", err)
+		logInstance.Error("Could not start server: %v", err)
 	}
 }
