77d2f5f1523967923dc145353c318aae4f50819b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2026-05-27 10:39:27 -0300

    feat: add movie details page

--- public/app.js
+++ public/app.js
@@ -1,13 +1,19 @@
 import { API } from "./services/API.js";
+import Router from "./services/Router.js";
 
 import "./components/HomePage.js";
 import "./components/AnimatedLoading.js";
 
+window.addEventListener("DOMContentLoaded", () => {
+    app.Router.init();
+});
+
 window.app = {
     search: (event) => {
         event.preventDefault();
-        const q = document.querySelector("input[type=search]").value;
+        const keywords = document.querySelector("input[type=search]").value;
     },
 
     api: API,
+    Router,
 };
